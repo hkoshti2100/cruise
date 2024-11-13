be60477f7b82d6b547a064c766e45349c1b5540a
Himanshu Koshti <hkoshti2022@gmail.com>
2024-11-13 18:57:54 +0530

    CREATE Premium Ad Component

---
+++ v1/frontend/src/components/Advertisement/AdPremium.tsx
@@ -0,0 +1,23 @@
+export default function PremiumAd() {
+	return (
+		<div id="ad-premium" className="container-ui">
+			<img
+				src="/images/premium-ad-banner.png"
+				alt="group-of-professional-people"
+				className="ad-image"
+			/>
+
+			<p className="ad-message">
+				Cruise along with other like minded people
+			</p>
+
+			<p className="ad-description text-small">
+				Join communities with people all across the world, now!
+			</p>
+
+			<p className="info-message text-small">
+				Only for $19.99
+			</p>
+		</div>
+	)
+}
--- v1/frontend/src/components/SideBar.tsx
+++ v1/frontend/src/components/SideBar.tsx
@@ -1,11 +1,12 @@
 import "@/styles/components/SideBar.scss"
+import AdPremium from "./Advertisement/AdPremium"
 
 export default function SideBar() {
 	return (
 		<aside id="sidebar-main">
 
 			<div id="user-profile" className="container-ui"></div>
-			<div id="ad-premium" className="container-ui"></div>
+			<AdPremium />
 
 		</aside>
 	)
